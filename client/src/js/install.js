ad5596fe609c5edf338e9b44035cdcce3c83bfdb
Shaun Nelson <shaunptnelson@gmail.com>
2023-07-11 12:15:09 -0400

    Added service worker functionality

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,7 +5,7 @@ const butInstall = document.getElementById("buttonInstall");
 window.addEventListener("beforeinstallprompt", (event) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   event.preventDefault();
-  // Stash the event so it can be triggered later.
+  // Stash the event so it can be triggered later
   window.deferredPrompt = event;
   // Update UI notify the user they can add to home screen
   butInstall.disabled = false;
@@ -15,13 +15,27 @@ window.addEventListener("beforeinstallprompt", (event) => {
 butInstall.addEventListener("click", async () => {
   // Hide the app provided install promotion
   butInstall.disabled = true;
+
   // Show the install prompt
   const { userChoice } = await window.deferredPrompt.prompt();
   console.log(userChoice);
+
+  // Wait for the user to respond to the prompt
+  if (userChoice.outcome === "accepted") {
+    console.log("User accepted the A2HS prompt");
+  }
+
+  // Clear the deferred prompt variable
+  window.deferredPrompt = null;
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  // Log install to analytics
   console.log("PWA was installed");
+
+  // Clear the deferredPrompt
+  window.deferredPrompt = null;
+
+  // Hide the install button
+  butInstall.disabled = true;
 });
